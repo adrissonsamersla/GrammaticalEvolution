9d88f449b3fdfa120af28981185273036c5ed653
Adriano <sadrianorod@gmail.com>
2019-10-27 19:46:14 -0300

    generate.py recebeu filtro contra erro por overflow e foi comentado

--- generate.py
+++ generate.py
@@ -1,9 +1,12 @@
 from random import randrange
 from math import exp,log,sqrt,cos,sin
 from readCSV import readCSV
+from Grammar import derivacao
+
+overFlowError = 1000000000000000
 
 def generateChromos(sizeChromo,rangeLimit):
-    
+    """ Função geradora dos cromossomos, com valores aleatórios entre [0,rangeLimit) """
     chromos = []
     
     for i in range(0,sizeChromo):
@@ -12,11 +15,11 @@ def generateChromos(sizeChromo,rangeLimit):
     return chromos
 
 def generateEquation(expression):
-    
+    """Função geradora de f(X) para a string inserida"""
     return lambda X1,X2,X3,X4,X5,X6,X7,X8: eval(expression)
 
 def generatePopulation(populationSize,sizeChromo,rangeLimit = 255):
-    ##############Pre-Sets###############
+    """Função geradora de população chromossomos, com tamanho igual a populationSize"""
     chromoList = []
 
     for single in range(0,populationSize):
@@ -25,6 +28,8 @@ def generatePopulation(populationSize,sizeChromo,rangeLimit = 255):
     return chromoList
 
 def generate_error_expression_list(data,chromoList):
+
+    """Função responsável por calcular o erro e devolver a expressão relativa ao erro calculado"""
 
     ##############Pre-Sets#############
     errorAverage = []  
@@ -36,17 +41,21 @@ def generate_error_expression_list(data,chromoList):
     for single in range(0,populationSize):
         
         error = []
-        
-        expressionList.append('X1*X2 + 3.5*exp(X1*X1)') #Recebe as expressão dependendo do cromossomo
+        expression = derivacao(chromoList[single]) #Calcula a expressão baseado na gramática pre determinada
+        expressionList.append(expression) #Recebe as expressão dependendo do cromossomo
         
         function = generateEquation(expressionList[single]) #Calcula a função
 
         for i in range(0,rows_data):
+            try:
+                [id,*args,f_real] = data[i].values()
+                e = abs((f_real - function(*args))**2)
             
-            [id,*args,f_real] = data[i].values()
-            e = abs((f_real - function(*args))**2)
+            except OverflowError:
+                e = overFlowError
+            
             error.append(e)
             
-        errorAverage.append((sum(error)/rows_data).round(5))
+        errorAverage.append((sum(error)/rows_data))
     
     return [errorAverage,expressionList]
